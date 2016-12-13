c2ceb855d72e9bfa0b0817e24d9cf1c91d4e6910
Joe Darby <joedarby87@gmail.com>
2016-12-13 19:58:08 +0000

    get ages

--- src/main/scala/spark/KMeans/SparkKMeans.scala
+++ src/main/scala/spark/KMeans/SparkKMeans.scala
@@ -9,31 +9,19 @@ object SparkKMeans {
     val sc = new SparkContext(new SparkConf().setAppName("Spark KMeans"))
     
     val lines = sc.textFile(args(0))
+    val ages = lines.map(getAge)
 
-    lines.foreach(println)
-    //val lines = sc.textFile("hdfs://moonshot-ha-nameservice" + args(0))
-
-    //val ages = lines.map(getAge)
-
-
-    //ages.foreach(println)
-
-
-
-    //transformations from the original input data
-    //val words = lines.flatMap(line => line.split("[ .,;:()]+"))
-    //val pairs = words.map(word => (word,1))
-    //val counts = pairs.reduceByKey((a,b)=>a+b)
-
-    //collect() is an action, so this value is retrieved to the driver
-    //val winter = counts.filter(pair=>pair._1.equals("winter")).collect()    
-    
-    //winter.foreach(println)
+    ages.foreach(println)
      
   }
 
   def getAge(line :String) : String = {
-    //return ((line.split("Age="))(1)).substring(1,3)
-    return (line.split("Age="))(1)
+    val fragments = line.split("\"")
+    var age = "0"
+    if (fragments.contains(" Age=")) {
+      val index = fragments.indexOf(" Age=")
+      age = fragments(index + 1)
+    }
+    return age
   }
 }
