a832da7a770b9f91e09751d0124108f5a6399f48
Joe Darby <joedarby87@gmail.com>
2016-12-14 00:12:49 +0000

    working feature parsing

--- src/main/scala/spark/KMeans/SparkKMeans.scala
+++ src/main/scala/spark/KMeans/SparkKMeans.scala
@@ -1,5 +1,7 @@
 package bdp.spark.KMeans
 
+import scala.collection.mutable.ListBuffer
+
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
@@ -7,28 +9,40 @@ import org.apache.spark.SparkConf
 object SparkKMeans {
   def main(args: Array[String]) {
     val sc = new SparkContext(new SparkConf().setAppName("Spark KMeans"))
-    
     val lines = sc.textFile(args(0))
     val featureSet = lines.map(getFeatures)
-
-    featureSet.foreach(println)
+    val printableFeatureSet = featureSet.map(makeListPrintable)
+    printableFeatureSet.foreach{println}
      
   }
 
-  def getFeatures(line :String) : [String] = {
-
-  	val featureIDs = [" rowID="," Reputation="," CreationDate="," LastAccessDate="," Views="," UpVotes="," DownVotes="," Age="]
+  def getFeatures(line :String) : List[String] = {
+    
     val fragments = line.split("\"")
-    var features = []
-
-    for (a <- 0 to 7) {
-    	if (fragments.contains(featureIDs(a))) {
-    		val index = fragments.indexOf(featureIDs(a))
-            features(a) = fragments(index + 1)
+   
+    val featureIDs = List(" Reputation="," CreationDate="," LastAccessDate="," Views="," UpVotes="," DownVotes="," Age=")
+    
+    var features = new ListBuffer[String]()
+    features += fragments(1)
+    
+    var a = ""
+    for (a <- featureIDs) {
+    	if (fragments.contains(a)) {
+    	    val index = fragments.indexOf(a)
+            features += fragments(index + 1)
         } else {
-        	features(a) = ""
+            features += ""
         }
     }
-    return features
+
+    features(2) = features(2).substring(0,10)
+    features(3) = features(3).substring(0,10)
+    
+    val featuresList = features.toList
+    return featuresList
+  }
+
+  def makeListPrintable(featureList : List[String]) : String = {
+        return featureList.mkString(", ")
   }
 }
