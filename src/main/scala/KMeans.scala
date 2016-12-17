9e491aa3afaff143e37d809858ccab916b0386e2
Joe Darby <joedarby87@gmail.com>
2016-12-17 20:06:14 +0000

    try to fix centre ordering problem

--- src/main/scala/KMeans.scala
+++ src/main/scala/KMeans.scala
@@ -9,7 +9,6 @@ object KMeans {
   /**
     * Run KMeans clustering on an input RDD vector
     */
-  //Create a map to store each data row with its closest cluster index as key
 
 
   def train(dataset : DataFrame, iterations:Int) : Unit = {
@@ -17,16 +16,8 @@ object KMeans {
     val m = 2
     val relevantData = dataset.select("Reputation", "Views")
     val rows = relevantData.rdd
-    //val rowsAsArray = rows.map(row => List(row.getInt(0).toFloat, row.getInt(1).toFloat, row.getInt(2).toFloat) )
     val rowsAsArray = rows.map(row => convertRow(row, m))
-    //val maximum = rowsAsArray.reduce((a, b) => math.max(a, b))
-    //println(maximum)
-    //rowsAsArray.foreach(println)
 
-    //number of intended clusters
-    //val n = rows.count() //number of datapoints
-     //number of features
-    //var centres = new ArrayBuffer[Float]()
 
     var centres: Array[Array[Float]] = rowsAsArray.takeSample(false, K, System.nanoTime().toInt)
     //To reduce chance of two random centres being the same, add a changing value to each
@@ -38,18 +29,6 @@ object KMeans {
       println("centre " + i + " = " + centres(i).mkString("[",",","]") )
     }
 
-    /*var centres : ArrayBuffer[Float] = new ArrayBuffer[Float](K)
-    for (i <- 0 until K) {
-      centres(i) = randomCentres(i)
-    }*/
-
-
-    /*var centres = new Array[Float](K)
-
-    val someNumber = maximum / K
-    for (i <- 0 until K) {
-      centres(i) = someNumber * i
-    }*/
 
     for (i <- 0 until iterations) {
       centres = clustering(centres, rowsAsArray, m, K)
@@ -64,29 +43,14 @@ object KMeans {
   def clustering(centres :Array[Array[Float]], rowsAsArray : RDD[Array[Float]], m : Int, K : Int) : Array[Array[Float]] = {
     val clusterMap :RDD[(Int,Array[Float])]= rowsAsArray.map(row => (assignCluster(row,centres,m,K),row))
     val newCentres = clusterMap.reduceByKey((a,b) => getMeanVector(a,b,m))
+    val arrayNewCentres = newCentres.collect()
 
-    val results = newCentres.map(x => x._2)
-    results.collect()
-
+    var results = new Array[Array[Float]](m)
+    for ((i,x) <- arrayNewCentres) {
+      results(i) = x
+    }
+    results
   }
-
-  //get random number generator r and use to select K centres randomly from dataset
-  /*val r = scala.util.Random
-   val random =
-  var a = 0
-  for (a <- 0 until K) {
-    centres(a) = rows(r.ne
-  }*/
-  //val centres = rowsAsArray.takeSample(false, K, System.nanoTime().toInt)
-  //val centres : Array[List[Float]] = Array(List(0.0f, 0.0f, 0.0f), List(10.0f, 10.0f, 10.0f), List(20.0f, 20.0f, 20.0f))
-
-  //val centre = 0.0f
-
-
-  //val newCentres = calculateNewCentres(clusterMap)
-  //val newCentre = rowsAsArray.reduce((a,b) => getAverage(a,b))
-
-
 
 
 
@@ -113,41 +77,6 @@ object KMeans {
     closestCentre
   }
 
-  /*
-
-    def calculateNewCentres(clusterMap : RDD[(Int,List[Float])]): RDD[(Int,List[Float])] = {
-      //val data = clusterMap.map(x => (x._1, x._2.asInstanceOf[ArrayBuffer[Double]]))
-      val newCentres = clusterMap.reduceByKey((a, b) => averageRow(a, b))
-      //val singleCluster = clusterMap.filter(x => x._1 == 0)
-      //val singleClusterAsArray = singleCluster.reduce()
-      newCentres
-    }*/
-
-
-
-
-  /*for (a <- 0 until K) {
-    var cluster = clusterMap.filter{case (a,_) => a == 0}
-    var data = cluster.map((_,a) => a :Row)*/
-
-
-
-  /*def getCentre(cluster : RDD[(Int,Row)], oldCentre : Row, clusterIndex :Int) : Row = {
-    val unWrappedData :RDD[Row] = cluster.map(x => x._2)
-    val features : Row = unWrappedData.reduce(averageRow)
-    return features
-  }*/
-
-  /*def averageRow(a :ArrayBuffer[Float], b:ArrayBuffer[Float]) : ArrayBuffer[Float] = {
-    val newRow = Row.apply()
-    for (i <- a.indices) {
-      val avgI = (a(i) + b(i)) /2
-      newRow(i) = avgI
-    }
-    newRow
-  }*/
-
-
   def averageRow(a:List[Float], b:List[Float]) : List[Float] = {
     val means = new ArrayBuffer[Float]
     for (i <- 0 until a.size) {
@@ -157,9 +86,6 @@ object KMeans {
     return means.toList
   }
 
-  /*def getAverage(a: Float, b:Float) : Float = {
-    return ((a+b)/2)
-  }*/
 
   def getMeanVector(a: Array[Float], b: Array[Float], m: Int) : Array[Float] = {
     var means = new Array[Float](m)
