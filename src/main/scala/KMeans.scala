498c6ba67a0ba2d9ea76b85b425154b6e8b0f097
Joe Darby <joedarby87@gmail.com>
2016-12-19 13:39:43 +0000

    add counts function

--- src/main/scala/KMeans.scala
+++ src/main/scala/KMeans.scala
@@ -12,9 +12,9 @@ object KMeans {
 
 
   def train(dataset : DataFrame, iterations:Int) : Unit = {
-    val K = 10
-    val m = 4
+    val K = 10 // Number of desired clusters
     val relevantData = dataset.select("Reputation", "Views", "UpVotes", "DownVotes")
+    val m = relevantData.columns.length  //number of features
     val rows = relevantData.rdd
     val rowsAsArray = rows.map(row => convertRow(row, m)).persist()
 
@@ -30,23 +30,53 @@ object KMeans {
     }
 
 
+    var counts = Array[Int](K)
+
+
+
     for (i <- 0 until iterations) {
-      centres = clustering(centres, rowsAsArray, m, K)
-      if (centres == null) {
+      val clusterMap = clustering(centres, rowsAsArray, m, K).persist()
+      centres = getCentres(clusterMap, m, K)
+      counts = getCounts(clusterMap, K)
+      clusterMap.unpersist()
+      if (centres == null || counts == null) {
         println("Error, starting again")
         train(dataset, iterations)
         return
       }
       println("\niteration " + i + " :")
       for (j <- 0 until K) {
-        println("centre " + j + " = " + centres(j).mkString("[",",","]") )
+        println("centre " + j + " = " + centres(j).mkString("[",",","]") + " count = " + counts(j) )
       }
     }
   }
 
 
-  def clustering(centres :Array[Array[Float]], rowsAsArray : RDD[Array[Float]], m : Int, K : Int) : Array[Array[Float]] = {
+  /*def clustering(centres :Array[Array[Float]], rowsAsArray : RDD[Array[Float]], m : Int, K : Int) : Array[Array[Float]] = {
+    val clusterMap :RDD[(Int,Array[Float])]= rowsAsArray.map(row => (assignCluster(row,centres,m,K),row)).persist()
+    val newCentres = clusterMap.reduceByKey((a,b) => getMeanVector(a,b,m))
+    val arrayNewCentres = newCentres.collect()
+
+    var results = new Array[Array[Float]](K)
+    for ((i,x) <- arrayNewCentres) {
+      results(i) = x
+    }
+    //Check all results are valid (no null)
+    for (i <- 0 until K) {
+      if (results(i) == null) {
+        return null
+      }
+    }
+    return results
+  }*/
+
+  def clustering(centres :Array[Array[Float]], rowsAsArray : RDD[Array[Float]], m : Int, K : Int) : RDD[(Int,Array[Float])] = {
     val clusterMap :RDD[(Int,Array[Float])]= rowsAsArray.map(row => (assignCluster(row,centres,m,K),row))
+    return clusterMap
+
+  }
+
+  def getCentres(clusterMap: RDD[(Int,Array[Float])], m: Int, K: Int) : Array[Array[Float]] = {
     val newCentres = clusterMap.reduceByKey((a,b) => getMeanVector(a,b,m))
     val arrayNewCentres = newCentres.collect()
 
@@ -63,6 +93,23 @@ object KMeans {
     return results
   }
 
+  def getCounts(clusterMap: RDD[(Int,Array[Float])], K: Int) : Array[Int] = {
+    val countable = clusterMap.map(x => (x._1, 1))
+    val countsWithKeys = countable.reduceByKey((a, b) => a + b).collect()
+    var counts = new Array[Int](K)
+    for ((i, x) <- countsWithKeys) {
+      counts(i) = x
+    }
+    for (i <- 0 until K) {
+      if (counts(i) == null) {
+        return null
+      }
+    }
+    return counts
+  }
+
+
+
 
 
     def calculateNorm(datapoint : Array[Float], centre : Array[Float], m: Int): Double = {
@@ -74,44 +121,35 @@ object KMeans {
       norm
     }
 
-  def assignCluster(row : Array[Float], centres: Array[Array[Float]], m : Int, K :Int): Int = {
-    var smallestNorm = 999999.0
-    var closestCentre = 0
-    for (centreNumber <- 0 until K) {
-      //val norm = Math.abs(row - centres(centreNumber))
-      val norm = calculateNorm(row, centres(centreNumber), m)
-      if (norm < smallestNorm) {
-        smallestNorm = norm
-        closestCentre = centreNumber
+    def assignCluster(row : Array[Float], centres: Array[Array[Float]], m : Int, K :Int): Int = {
+      var smallestNorm = 999999.0
+      var closestCentre = 0
+      for (centreNumber <- 0 until K) {
+        //val norm = Math.abs(row - centres(centreNumber))
+        val norm = calculateNorm(row, centres(centreNumber), m)
+        if (norm < smallestNorm) {
+          smallestNorm = norm
+          closestCentre = centreNumber
+        }
       }
+      closestCentre
     }
-    closestCentre
-  }
 
-  def averageRow(a:List[Float], b:List[Float]) : List[Float] = {
-    val means = new ArrayBuffer[Float]
-    for (i <- 0 until a.size) {
-      val mean = (a(i) + b(i)) /2.0f
-      means(i) = mean
+
+    def getMeanVector(a: Array[Float], b: Array[Float], m: Int) : Array[Float] = {
+      var means = new Array[Float](m)
+      for (i <- 0 until m) {
+        means(i) = (a(i) + b(i)) / 2
+      }
+      means
     }
-    return means.toList
-  }
 
-
-  def getMeanVector(a: Array[Float], b: Array[Float], m: Int) : Array[Float] = {
-    var means = new Array[Float](m)
-    for (i <- 0 until m) {
-      means(i) = (a(i) + b(i)) / 2
+    def convertRow(row : Row, m: Int) : Array[Float] = {
+      var dataArray = new Array[Float](m)
+      for (i <- 0 until m) {
+        dataArray(i) = row.getInt(i).toFloat
+      }
+      dataArray
     }
-    means
-  }
 
-  def convertRow(row : Row, m: Int) : Array[Float] = {
-    var dataArray = new Array[Float](m)
-    for (i <- 0 until m) {
-      dataArray(i) = row.getInt(i).toFloat
-    }
-    dataArray
   }
-
-}
